eac590e187bdc7d50cfc1941cdb54480b0380a03
Shuaib <shuaibakoh6@gmail.com>
2024-05-01 15:42:53 +0100

    moved profile icon to header

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,46 +1,50 @@
+"use client"
+
+import { sidebarLinks } from "@/constants";
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 function Topbar() {
+  const pathname = usePathname();
+
   return (
     <nav className="topbar">
       <a href="/">
         <Image src="/assets/logo1.svg" alt="logo" width={64} height={64} />
       </a>
-
-      {/* <p className='text-heading3-bold text-light-1 max-xs:hidden'>Topicanon</p> */}
-
-      <div className="flex items-center gap-1">
-        <div className="block md:hidden">
-          <div className="mt-10 px-6">
-            {/* <SignedIn>
-          <SignOutButton redirectUrl="/sign-in">
-            <div className='flex cursor-pointer gap-4 p-4'>
-              <Image
-                src='/assets/logout.svg'
-                alt='logout'
-                width={24}
-                height={24}
-              />
-
-              <p className='text-light-2 max-lg:hidden'>Logout</p>
-            </div>
-          </SignOutButton>
-        </SignedIn> */}
-          </div>
+      
+      <div className='bottombar_container '>
+          {sidebarLinks.map((link) => {
+            if (link.label === "Profile") {
+              const isActive =
+                (pathname.includes(link.route) && link.route.length > 1) ||
+                pathname === link.route;
+    
+              return (
+                <Link
+                  href={link.route}
+                  key={link.label}
+                  className={`bottombar_link ${isActive && "bg-dark-3"}`}
+                >
+                  <Image
+                    src={link.imgURL}
+                    alt={link.label}
+                    width={24}
+                    height={24}
+                    className='object-contain'
+                  />
+    
+                  <p className='text-subtle-medium text-light-1 max-sm:hidden'>
+                    {link.label.split(/\s+/)[0]}
+                  </p>
+                </Link>
+              );
+            }
+          })}
         </div>
-
-        <OrganizationSwitcher
-          appearance={{
-            baseTheme: dark,
-            // elements: {
-            //   organizationSwitcherTrigger: "py-2 px-4",
-            // },
-          }}
-        />
-      </div>
     </nav>
   );
 }
