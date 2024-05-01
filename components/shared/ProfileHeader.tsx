28811a1b14af2c9ee33221e87e69d3d41e2c9eb0
Shuaib <shuaibakoh6@gmail.com>
2024-05-01 10:13:31 +0100

    added gap to profileheader

--- components/shared/ProfileHeader.tsx
+++ components/shared/ProfileHeader.tsx
@@ -22,7 +22,7 @@ function ProfileHeader({
 }: Props) {
   return (
     <div className="flex w-full flex-col justify-start">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center gap-3 justify-between">
         <div className="flex items-center gap-3">
           <div className="relative h-20 w-20 object-cover">
             <Image
